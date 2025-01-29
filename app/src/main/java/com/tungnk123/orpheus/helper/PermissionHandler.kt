1bb529e1e39fcc81d5b105b8595788102d3563d4
tungnk123 <doanthanhtungnk123@gmail.com>
2025-01-29 17:33:35 +0700

    Add PermissionHandler helper class

---
+++ app/src/main/java/com/tungnk123/orpheus/helper/PermissionHandler.kt
@@ -0,0 +1,48 @@
+package com.tungnk123.orpheus.helper
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.activity.result.contract.ActivityResultContracts
+import com.tungnk123.orpheus.MainActivity
+
+class PermissionHandler {
+    data class State(
+        val required: List<String>,
+        val granted: List<String>,
+        val denied: List<String>,
+    ) {
+        fun hasAll() = denied.isEmpty()
+    }
+
+    fun handle(activity: MainActivity) {
+        val state = getState(activity)
+        if (state.hasAll()) {
+            return
+        }
+        val contract = ActivityResultContracts.RequestMultiplePermissions()
+        activity.registerForActivityResult(contract) {}.launch(state.denied.toTypedArray())
+    }
+
+    private fun getRequiredPermissions(): List<String> {
+        val required = mutableListOf<String>()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            required.add(Manifest.permission.POST_NOTIFICATIONS)
+        }
+        return required
+    }
+
+    private fun getState(activity: MainActivity): State {
+        val required = getRequiredPermissions()
+        val granted = mutableListOf<String>()
+        val denied = mutableListOf<String>()
+        required.forEach {
+            if (activity.checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED) {
+                granted.add(it)
+            } else {
+                denied.add(it)
+            }
+        }
+        return State(required = required, granted = granted, denied = denied)
+    }
+}
