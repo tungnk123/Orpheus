3a7f7f0748b150a844c9160e0a0d6e9c71b89fad
tungnk123 <doanthanhtungnk123@gmail.com>
2025-02-11 15:52:07 +0700

    Fix bugs ksp with hilt compiler dependency
    3 days :))

--- app/src/main/java/com/tungnk123/orpheus/ui/MainViewModel.kt
+++ app/src/main/java/com/tungnk123/orpheus/ui/MainViewModel.kt
@@ -1,9 +1,10 @@
-//package com.tungnk123.orpheus.ui
-//
-//import androidx.lifecycle.ViewModel
-//import dagger.hilt.android.lifecycle.HiltViewModel
-//import javax.inject.Inject
-//
-//@HiltViewModel
-//class MainViewModel @Inject constructor() : ViewModel() {
-//}
+package com.tungnk123.orpheus.ui
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
+    val temp = listOf(1, 2)
+}
