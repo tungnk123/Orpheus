f878449db74c60c3af0d1eb02967d032dc94371d
tungnk123 <doanthanhtungnk123@gmail.com>
2025-02-15 22:54:48 +0700

    Update PermissionHandler and use it in MainActivity

--- app/src/main/java/com/tungnk123/orpheus/MainActivity.kt
+++ app/src/main/java/com/tungnk123/orpheus/MainActivity.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.tungnk123.orpheus.helper.PermissionHandler
 import com.tungnk123.orpheus.ui.OrpheusApp
 import com.tungnk123.orpheus.ui.theme.OrpheusTheme
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var permissionHandler: PermissionHandler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        permissionHandler.handle(this)
         setContent {
             OrpheusTheme {
                 OrpheusApp()
