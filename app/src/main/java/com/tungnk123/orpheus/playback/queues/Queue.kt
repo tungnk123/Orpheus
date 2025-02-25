0a9bd08380900c92dab14633417861270b7f6e5e
tungnk123 <doanthanhtungnk123@gmail.com>
2025-02-25 22:16:37 +0700

    Add queue and SleepTimer for playback

---
+++ app/src/main/java/com/tungnk123/orpheus/playback/queues/Queue.kt
@@ -0,0 +1,21 @@
+package com.tungnk123.orpheus.playback.queues
+
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MediaMetadata
+
+interface Queue {
+    val preloadItem: MediaMetadata?
+
+    suspend fun getInitialStatus(): Status
+
+    fun hasNextPage(): Boolean
+
+    suspend fun nextPage(): List<MediaItem>
+
+    data class Status(
+        val title: String?,
+        val items: List<MediaItem>,
+        val mediaItemIndex: Int,
+        val position: Long = 0L,
+    )
+}
